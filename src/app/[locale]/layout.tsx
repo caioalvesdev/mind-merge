c593047492ccabebfad9c6b17c81be560ee193b0
Caio Alves <ktinsecrx2@gmail.com>
2024-02-10 14:23:06 -0300

    fix: fix rootlayout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,7 +8,6 @@ import "./globals.css";
 import { CrispProvider } from "@/components/Crisp-provider";
 import { ThemeProvider } from "@/components/Theme-provider"
 import { NextIntlClientProvider } from 'next-intl'
-import { dark, neobrutalism } from '@clerk/themes';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,18 +28,15 @@ export default async function RootLayout({
 }: Readonly<IRootLayoutProps>) {
   let messages = (await import(`../../../messages/${locale}.json`)).default
   return (
-    <ClerkProvider 
-     
-      localization={locale === 'pt' ? ptBR : undefined}>
+    <ClerkProvider localization={locale === 'pt' ? ptBR : undefined}>
       <NextIntlClientProvider locale={locale} messages={messages}>
-      <html lang="en">
+      <html lang="pt-BR">
         <CrispProvider />
         <body className={inter.className}>
         <ThemeProvider
             attribute="class"
             defaultTheme="light"
             enableSystem
-            
           >
             <ModalProvider />
             {children}
